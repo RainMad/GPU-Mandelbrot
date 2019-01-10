12af54325071a3273cea2ce4b6cc53b407fb811b
Rainer Madlmayr <s1710595011@students.fh-hagenberg.at>
2019-01-10 22:33:17 +0100

    Modified the saving process of the images

--- MandelbrotGPU/main.cpp
+++ MandelbrotGPU/main.cpp
@@ -42,7 +42,6 @@ int main() {
 
 		double const zoom_factor = 0.95;
 
-
 		int const iterations = 30;
 		int const threshold = 4;
 
@@ -54,28 +53,24 @@ int main() {
 		auto & span{ bmp.pixel_span() };
 		auto * const p_buffer{ std::data(span) };
 
-		int p_buffer_size = bitmap_width * bitmap_height * sizeof(pfc::BGR_4_t)*amount_of_images;
+		int p_buffer_size = bitmap_width * bitmap_height * sizeof(pfc::BGR_4_t);
 		pfc::BGR_4_t * bmp_dst{}; cudaMalloc(&bmp_dst, p_buffer_size);
 
 
 		std::unique_ptr< pfc::BGR_4_t[]>			hp_dst{ std::make_unique <pfc::BGR_4_t[]>(p_buffer_size) };
 
 		pfc::bitmap bmpCp{ bitmap_width, bitmap_height };
-		//check(cudaMemcpy(bmp_src, p_buffer, p_buffer_size, cudaMemcpyHostToDevice));
+
 		for (int k = 0; k < amount_of_images; k++) {
 			check(call_kernel(bitmap_width *bitmap_height / 32 + 1, 32, bmp_dst, bitmap_width *bitmap_height, imag_max, imag_min, real_max, real_min, threshold, iterations, bitmap_width, bitmap_height, amount_of_images, point_real, point_imag, zoom_factor, k));
 
 			check(cudaMemcpy(hp_dst.get(), bmp_dst, p_buffer_size, cudaMemcpyDeviceToHost));
 
-		for (int i = 0; i < bitmap_width *bitmap_height; i++) {
-				auto val = hp_dst[i];
-				auto val1 = hp_dst.get()[i];
-				pfc::byte_t r = hp_dst.get()[i].red;
-				pfc::byte_t g = hp_dst.get()[i].green;
-				pfc::byte_t b = hp_dst.get()[i].blue;
-				bmpCp.pixel_span()[i] = { r, g, b };//hp_dst.get()[i];
+			for (int i = 0; i < bitmap_width *bitmap_height; i++) {
+				bmpCp.pixel_span()[i] = { hp_dst.get()[i] };
 			}
-			bmpCp.to_file("./test" + std::to_string(k) + ".bmp");
+		
+			bmpCp.to_file("./images/test" + std::to_string(k) + ".bmp");
 		}
 
 		check(cudaFree(bmp_dst));
